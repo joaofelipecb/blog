6ee08c7271d487a6d17ddb1b8d6f56f8c08a8575
João Felipe Chiarelli Bourscheid <joao.felipe.c.b@gmail.com>
2021-06-09 17:24:00 -0300

    created db schema

---
+++ schema.sql
@@ -0,0 +1,18 @@
+drop table if exists user;
+drop table if exists post;
+
+create table user (
+	user_id integer primary key autoincrement,
+	user_username text unique not null,
+	user_password text not null,
+);
+
+create table post (
+	post_id integer primary key autoincrement,
+	user_id integer not null,
+	post_created timestamp not null default current_timestamp,
+	post_title text not null,
+	post_body text not null,
+	foreign key (user_id) references user (user_id)
+);
+
