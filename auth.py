11409e0cbfc17f8ade344c81be1e5d804361333d
João Felipe Chiarelli Bourscheid <joao.felipe.c.b@gmail.com>
2021-06-09 22:06:29 -0300

    added a load_logged_in_user function

--- auth.py
+++ auth.py
@@ -58,3 +58,13 @@ def login():
 
     return render_template('auth/login.html')
         
+@bp.before_app_request
+def load_logged_in_user():
+    user_id = session.get('user_id')
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = get_db().execute(
+                'select * from user where user_id = ?', (user_id)
+        ).fetchone()
+
