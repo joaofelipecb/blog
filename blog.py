df3d2cb14ceb302e6ff1294ca03eb06fbf18a0ed
João Felipe Chiarelli Bourscheid <joao.felipe.c.b@gmail.com>
2021-06-11 09:39:13 -0300

    included a blog create view

--- blog.py
+++ blog.py
@@ -20,3 +20,23 @@ def index():
    )
     return render_template('blog/index.html', posts=posts)
 
+@bp.route('/create', methods=('GET', 'POST'))
+@login_required
+def create():
+    if request.method == 'POST':
+        title = request.form['title']
+        body = request.form['body']
+        error = None
+
+        if not title:
+            error = 'Title is required.'
+        else:
+            db = get_db()
+            db.execute(
+'''insert into post (post_title, post_body, user_id)
+values (?, ?, ?)''',
+            (title, body, g.user['id'])
+            )
+            return redirect(url_for('blog.index'))
+        return render_template('blog/create.html')
+
