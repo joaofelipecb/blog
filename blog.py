713486791810dfb88effa37cf192e22ba06bd32b
João Felipe Chiarelli Bourscheid <joao.felipe.c.b@gmail.com>
2021-06-11 10:33:38 -0300

    included blog update view

--- blog.py
+++ blog.py
@@ -40,3 +40,44 @@ values (?, ?, ?)''',
             return redirect(url_for('blog.index'))
         return render_template('blog/create.html')
 
+def get_post(id, check_author=True):
+    post = get_db().execute(
+'''select post_id, post_title, post_body, post_created, user_id, user_username
+from post
+natural join user
+where post_id = ?''').fetchone()
+    
+    if post is None:
+        abort(404, f"Post id {id} doesn't exist.")
+
+    if check_author and post['user_id'] != g.user['user_id']:
+        abort(403)
+
+    return post
+
+@bp.route('/<int:id>/update', methods=('GET','POST'))
+@login_required
+def update(id):
+    post = get_post(id)
+
+    if request.method == 'POST':
+        title = request.form['title']
+        body = request.form['body']
+        error = None
+
+        if not title:
+            error = 'Title is required'
+
+        if error is not None:
+            flash(error)
+        else:
+            db = get_db()
+            db.execute(
+'''update post set post_title = ?, body = ?
+where id = ?''',
+                (title, body, id)
+            )
+            db.commit()
+            return redirect(url_for('blog.index'))
+        return render_template('blog/update.html', post=post)
+
