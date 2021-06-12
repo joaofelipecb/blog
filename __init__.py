1d2822be156881669799fdf250d92ab154c14fe0
João Felipe Chiarelli Bourscheid <joao.felipe.c.b@gmail.com>
2021-06-12 19:03:10 -0300

    created a load config from xhtml

--- __init__.py
+++ __init__.py
@@ -1,18 +1,37 @@
 import os
 from flask import Flask
 
+def load_config():
+    import xml.etree.ElementTree as ET
+    tree = ET.parse('p17data/config.html')
+    articles = tree.findall('body/article')
+    config = {}
+    for article in articles:
+        sections = article.findall('section')
+        for section in sections:
+            configName = section.find('header/hgroup/h1').text
+            paragraph = section.find('section/p')
+            definitions = section.findall('section/dl/*')
+            if paragraph is not None:
+                config[configName] = paragraph.text
+            elif definitions:
+                config[configName] = {}
+                for definition in definitions:
+                    if definition.tag == 'dt':
+                        definitionName = definition.text
+                    elif definition.tag == 'dd':
+                        config[configName][definitionName] = definition.text
+    return config
+
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
+    
+    app.config.update(load_config())
 
-    app.config.from_mapping(
-            SECRET_KEY='dev',
-            DATABASE=os.path.join(app.instance_path, 'blog.sqlite'),
-    )
-
-    if test_config is None:
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        app.config.from_mapping(test_config)
+#    app.config.from_pyfile('p17data/config.py', silent=True)
+#            SECRET_KEY='dev',
+#            DATABASE=os.path.join(app.instance_path, 'blog.sqlite'),
+#    )
 
     try:
         os.makedirs(app.instance_path)
